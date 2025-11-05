752bba225255e51876cb0a37aed3171f9c3cfa3e
jevvonn <darmawanb985@gmail.com>
2025-11-05 11:50:58 +0700

    feat: integrate TRPC and React Query for API handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { TRPCProvider } from "@/trpc/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TRPCProvider>{children}</TRPCProvider>
         <Toaster />
       </body>
     </html>
